7aaf6afaa56bc53606077b2f3584c82b8a7d36a8
ger121020 <germanC121020@gmail.com>
2022-04-15 17:20:36 -0600

    Variables

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,10 @@
 const { src, dest, watch } = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
+const plumber = require('gulp-plumber');
 
 function css ( done){
     src('src/scss/**/*.scss') //identificar el archivo scss a compilar
+        .pipe(plumber())
         .pipe(sass()) // compilarlo
         .pipe(dest('build/css'))
 
